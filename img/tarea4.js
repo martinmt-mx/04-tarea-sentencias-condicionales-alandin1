4f0ceb85bf5bebbf8f7758c79b5242772cb74926
Ana Landin <alandin1@ucol.mx>
2019-10-03 01:03:12 -0500

    css

--- img/tarea4.js
+++ img/tarea4.js
@@ -1,6 +1,7 @@
 // Ejercicio 1
 var ejer1Num1 = document.querySelector('#ejer1Num1')
 var btnEj1 = document.querySelector('#btnEj1')
+
 btnEj1.addEventListener('click', () =>{
     let num1 = Number(ejer1Num1.value)
     let residuo = num1%2
@@ -47,48 +48,5 @@ btnEj2.addEventListener('click', () =>{
     else if (num3<num1 && num3<num2)
     {
         alert(num3 + " Es menor")
-    }
-})
-
-// Ejercicio 3
-var ejer3Num1 = document.querySelector('#ejer3Num1')
-var ejer3Num2 = document.querySelector('#ejer3Num2')
-var btnEj3 = document.querySelector('#btnEj3')
-btnEj3.addEventListener('click', () => {
-    let saldo = Number(ejer3Num1.value)
-    let retiro = Number(ejer3Num2.value)
-    if (saldo<retiro)
-    {
-        alert("No tienes suficiente saldo")
-    }
-    else
-    {
-        saldo = saldo - retiro
-        alert("Retiro exitoso!, su nuevo saldo es: " + saldo + "$")
-    }
-})
-
-// Ejercicio 4
-var ejer4Num1 = document.querySelector('#ejer4Precio')
-var ejer4Cantidad = document.querySelector('#ejer4Cantidad')
-var btnPrecio = document.querySelector('#btnPrecio')
-var ejer4Result = document.querySelector('#ejer4Result')
-btnPrecio.addEventListener('click', () => {
-    let precio = Number(ejer4Precio.value)
-    let cantidad = Number(ejer4Cantidad.value)
-    if (cantidad<=9)
-    {
-        precio = (cantidad*precio)* .95
-        ejer4Result.value = precio
-    }
-    if (cantidad>=10 && cantidad<=20)
-    {
-        precio = (cantidad*precio)* .90
-        ejer4Result.value = precio
-    }
-    if (cantidad>=21)
-    {
-        precio = (cantidad*precio)* .85
-        ejer4Result.value = precio
     }
 })
